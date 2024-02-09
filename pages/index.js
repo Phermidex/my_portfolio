cdcf653148fa6aa9eb4e96584adc086fbe9cb9b0
Aih <110110613+Aihcode@users.noreply.github.com>
2024-02-09 12:04:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -103,11 +103,11 @@ export default function Home() {
         <title>Hermides Medina | My portfolio</title>
         <meta name="description" content="Present my portfolio" />
         <link rel="icon" href="./favicon.ico" />
+        <link rel="stylesheet" href={`https://fonts.googleapis.com/css2?family=Grand+Hotel&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400&display=swap`}/>
       </Head>
       <Navigation />
       <Container fluid>
-      <link rel="stylesheet" href={`https://fonts.googleapis.com/css2?family=Grand+Hotel&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400&display=swap`}/>
-        <main className="main home">
+      <main className="main home">
           <div className="main__title--board">
             <Particles
               id="tsparticles"
