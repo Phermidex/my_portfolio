a37e5efbb41b86b21f44c39a6c4469ace067ca12
Hermides Miguel Medina Morales <hermides07@gmail.com>
2024-02-08 20:24:58 -0400

    Add all my changes and updates
    Perfomance applied

---
+++ pages/components/footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer(props) {
+
+    const { styles } = props;
+
+  return (
+    <footer className="footer">
+      <a
+        href="https://github.com/Phermidex"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        &copy; 2022 All reserved Hermides Medina | Designed By Phermidex.
+      </a>
+    </footer>
+  );
+}
